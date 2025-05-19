665af9528fcd2dbc150a2ca8d47387f5587ad779
alasiri <austinalasiri44@gmail.com>
2025-05-19 11:17:36 +0300

    all commits

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Yogi', 12, 'M', 'brown', 'playful', 'true'),
+('Boo Boo', 8, 'M', 'grey', 'nervous', 'true'),
+('Cinnamon', 15, 'F', 'cinnamon', 'gentle', 'true'),
+('Grizzly', 20, 'M', 'dark brown', 'aggressive', 'true'),
+('Snowball', 10, 'F', 'white', 'calm', 'true'),
+('Honey', 9, 'F', 'golden', 'friendly', 'false'),
+('Shadow', 13, 'M', 'black', 'quiet', 'true'),
+(NULL, 7, 'F', 'light brown', 'curious', 'true');
